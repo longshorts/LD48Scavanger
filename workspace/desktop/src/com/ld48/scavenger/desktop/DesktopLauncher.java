33f8a3bb09c706690129cc515062fe861cbe511b
Andrew Nash <andrew@andrewoid.co.uk>
2014-04-26 13:47:41 +0100

    Initial Commit - Workspace & Art
    This contains the workspace and first lot of art for Scavenger

---
+++ workspace/desktop/src/com/ld48/scavenger/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ld48.scavenger.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ld48.scavenger.ScavengerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ScavengerGame(), config);
+	}
+}
---
+++ workspace/html/src/com/ld48/scavenger/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.ld48.scavenger.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.ld48.scavenger.ScavengerGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new ScavengerGame();
+        }
+}
